ed877a11230fa142dd14dba86966bc6d3faf45cf
DebabrataDKD <devkumr2323@gmail.com>
2025-02-20 17:11:48 +0530

    Updated RestAssuredAPITesting with latest changes

--- src/test/java/testing/TC01.java
+++ src/test/java/testing/TC01.java
@@ -1,16 +1,17 @@
-package com.debu.ra01;
+package testing;
 
 import io.restassured.RestAssured;
+import org.testng.annotations.Test;
 
+public class TC01 {
 
-public class Test01 {
-    public static void main(String[] args) {
-        //rest assured hello program
+    @Test
+    public void getRequest(){
         RestAssured.given().baseUri("https://restful-booker.herokuapp.com")
                 .basePath("/ping")
                 .when().get()
-               .then().statusCode(201);
-
-
+                .then().statusCode(201);
     }
-}
+      }
+
+
